4e831b48edf6adec9ab775aabad6f32028c83cdc
Ivan Ninov <ivan@ninov.org>
2025-07-02 23:04:34 +0300

    third problem fast food sequence orders

---
+++ List_Stack_Queues/fast_food.py
@@ -0,0 +1,16 @@
+from collections import deque
+quantity_of_food = int(input())
+sequence = deque([int(x) for x in input().split()])
+print(f"{max(sequence)}")
+
+total_sum = 0
+while sequence:
+    if (total_sum + sequence[0]) > quantity_of_food:
+        print("Orders left: ", end="")
+        print(*sequence, sep=" ")
+        break
+    total_sum += sequence.popleft()
+
+if not sequence:    
+    print("Orders complete")
+
