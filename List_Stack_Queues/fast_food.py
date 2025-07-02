2b299fcfcc16554541fba1de9c627708625e6a68
Ivan Ninov <ivan@ninov.org>
2025-07-02 23:07:59 +0300

    fixing issue

--- List_Stack_Queues/fast_food.py
+++ List_Stack_Queues/fast_food.py
@@ -5,7 +5,7 @@ print(f"{max(sequence)}")
 
 total_sum = 0
 while sequence:
-    if (total_sum + sequence[0]) > quantity_of_food:
+    if (total_sum + sequence[0]) >= quantity_of_food:
         print("Orders left: ", end="")
         print(*sequence, sep=" ")
         break
