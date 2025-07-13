26efd2b213f9234f6c060a7bf6d01d2e2884bafd
Ivan Ninov <ivan@ninov.org>
2025-07-13 16:32:08 +0300

    Adding Diagonals sum diff

--- multi_dim_arrays/Diagonals.py
+++ multi_dim_arrays/Diagonals.py
@@ -1,8 +1,18 @@
-n = int(input())
+# n = int(input())
 
-matrix = [[int(x) for x in input().split(", ")]  for _ in range(n)]
-primary  = [matrix[r][r] for r in range(n)]
-secondary = [matrix[r][n-r-1] for r in range(n)]
-print(f"Primary diagonal: {', '.join(str(x) for x in primary)}. Sum: {sum(primary)}")
-print(f"Secondary diagonal: {', '.join(str(x) for x in secondary)}. Sum: {sum(secondary)}")
+# matrix = [[int(x) for x in input().split(", ")]  for _ in range(n)]
+# primary  = [matrix[r][r] for r in range(n)]
+# secondary = [matrix[r][n-r-1] for r in range(n)]
+# print(f"Primary diagonal: {', '.join(str(x) for x in primary)}. Sum: {sum(primary)}")
+# print(f"Secondary diagonal: {', '.join(str(x) for x in secondary)}. Sum: {sum(secondary)}")
 
+n2 = int(input())
+matrix2 = [[int(x) for x in input().split()]  for _ in range(n2)]
+
+primary_sum, secondary_sum = 0,0
+
+for row in range(n2):
+    primary_sum += matrix2[row][row]
+    secondary_sum += matrix2[row][n2-row-1]
+
+print(abs(primary_sum - secondary_sum))
