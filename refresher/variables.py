a063d8aaf324834bbcf3c85a1e09caca03cdaf36
Ivan Ninov <ivan@ninov.org>
2025-11-28 16:28:24 +0200

    Refactor variable initialization and update print statements for clarity

--- refresher/variables.py
+++ refresher/variables.py
@@ -1,2 +1,12 @@
+import math
+
+
 x = 5
-print("The value of x is:", x)
+y,z = (1,2)
+a,b,c,d = 10, 20, 30, 40
+e = 4.755644545
+
+print(f"The value of x is: {x}")
+print(f"The values of y and z are: {y}, {z}")
+print(f"{math.floor(e*100)/100:.2f}")
+print(f"{e:.2f}")
