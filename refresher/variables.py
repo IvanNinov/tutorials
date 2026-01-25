c1acad546b45324109a0dde7a6e806a753a626f3
IvanNinov <ivan@ninov.org>
2026-01-25 15:09:45 -0500

    final

--- refresher/variables.py
+++ refresher/variables.py
@@ -5,7 +5,6 @@ x = 5
 y,z = (1,2)
 a,b,c,d = 10, 20, 30, 40
 e = 4.755644545
-
 print(f"The value of x is: {x}")
 print(f"The values of y and z are: {y}, {z}")
 print(f"{math.floor(e*100)/100:.2f}")
