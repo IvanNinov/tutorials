968724fe03215eaa49f026e532d06eb4dc89addf
Ivan Ninov <ivan@ninov.org>
2026-04-25 15:00:10 +0300

    refresh skills

--- refresher/variables.py
+++ refresher/variables.py
@@ -10,3 +10,8 @@ print(f"The value of x is: {x}")
 print(f"The values of y and z are: {y}, {z}")
 print(f"{math.floor(e*100)/100:.2f}")
 print(f"{e:.2f}")
+
+x, y = True, False
+print((x or y) == True)
+print((x and y) == False)
+print((not y) == True)
